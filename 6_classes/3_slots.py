29e340a7862d66d238d2da7264a60e680227a236
Maxim Tronov <tronov.m.a@gmail.com>
2017-07-11 22:50:18 +0300

    Add comments

--- 6_classes/3_slots.py
+++ 6_classes/3_slots.py
@@ -1,5 +1,5 @@
 class Noop:
-    __slots__ = ["some_attribute"]
+    __slots__ = ["some_attribute"]  # that is magic attribute which defines the class attributes dict
 
 noop = Noop()
 noop.some_attribute = 42
@@ -7,5 +7,12 @@ print(noop.some_attribute)
 
 noop.other_attribute = 100500
 
+# you can't set attribute which is not defined in __slots__
+
+# yet another way to get the class attribute
+print(Noop.some_attribute.__get__(noop))
+
 # instances of classes which have __slots__ dict attribute
 # require less memory because they haven't __dict__
+
+# Usually you should use namedtuple instead of class with __slots__
--- 6_classes/5_properties.py
+++ 6_classes/5_properties.py
@@ -18,15 +18,20 @@ class BigDataModel:
     def __init__(self):
         self._params = []
 
+    # This is a getter for the params property
     @property
     def params(self):
         return self._params
 
+    # This is a setter for the params property
+    # It is optional
     @params.setter
     def params(self, new_params):
         assert all(map(lambda p: p > 0, new_params))
         self._params = new_params
 
+    # This is a deleter for the params property
+    # It is also optional
     @params.deleter
     def params(self):
         del self._params
